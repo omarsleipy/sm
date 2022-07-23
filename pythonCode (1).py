9bb36767d6e9a3e16e4b9a1ca72f6d193bd0a81c
omarsleipy <109833393+omarsleipy@users.noreply.github.com>
2022-07-23 21:46:31 +0300

    Add files via upload

---
+++ pythonCode (1).py
@@ -0,0 +1,20 @@
+from gtts import gTTS
+from playsound import playsound
+import speech_recognition as sr
+r = sr.Recognizer()
+with sr.Microphone() as src:
+    print('Say something....')
+    audio = r.listen(src)
+try:
+    t = r.recognize_google(audio, language='ar-AR')
+    print(t)
+    f = open('text.txt', 'a', encoding='utf-8')
+    f.writelines(t+'\n')
+    f.close()
+    obj = gTTS(text=t, lang='ar', slow=False)
+    obj.save('text.mp3')
+    playsound('text.mp3')
+except sr.UnknownValueError as U:
+    print(U)
+except sr.RequestError as R:
+    print(R)
